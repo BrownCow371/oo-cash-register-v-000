58476b0e6769319357dcf61141a70bb1e37e913b
Jamie Brown <jmbrowncow@gmail.com>
2018-07-09 17:57:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
 
   def add_item(item, price, quantity=1)
       @items<<item
-      @total=+(price*quantity)
+      self.total = @total=+(price*quantity)
   end
 
   def apply_discount
