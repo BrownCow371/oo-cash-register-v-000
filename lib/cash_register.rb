eec86e0863385445067d6fcde023334e91f5b2cb
Jamie Brown <jmbrowncow@gmail.com>
2018-07-09 17:51:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
+  attr_accessor :total :discount
 
+
+
+  def initialize(discount=0)
+    @total=0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(item, price, quantity=1)
+    @total=+price*quantity
+    @items<<item
+  end
+
+  def apply_discount
+     @discount>0 ? @total*discount% : @total
+  end
+end
